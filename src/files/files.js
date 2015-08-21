e9bae7175157f58a69a2c55f9993ff3757be7a57
missingdays <daysaremissing@gmail.com>
2015-08-21 22:06:54 +0300

    Add changeDir function

--- src/files/files.js
+++ src/files/files.js
@@ -1,9 +1,12 @@
 var fs = require("fs");
+var path = require("path");
 var logger = require("ed3-logger");
 
 var parser = require("../parser/parser.js");
 
-function parseFile(fileName, callback){
+var files = {};
+
+files._parseFile = function(fileName, callback){
     fs.readFile(fileName, { encoding: 'utf8' }, function(err, data){
 
         var parsedFile = parser.parse(data);
@@ -14,7 +17,7 @@ function parseFile(fileName, callback){
     });
 }
 
-function changeExtension(fileName){
+files._changeExtension = function(fileName){
     var arr = fileName.split(".");
 
     var ext = arr[arr.length-1];
@@ -28,16 +31,58 @@ function changeExtension(fileName){
     return arr.join(".");
 }
 
-function parse(fileName){
+function removePrePostFixes(path){
+    if(!path[0]) path.shift();
+    if(path.length > 0 && !path[path.length-1]) path.pop();
+    return path;
+}
+
+files._changeDir = function(file, options){
+    var self = this;
+
+    var splt = file.split("/");
+
+    if(splt.length < 2){
+        return path.normalize(file);
+    }
+
+    var options = options || {};
+
+    var from = options.from || self.from || "jst";
+    var to = options.to || self.to || "js";
+
+    from = from.split("/");
+    to = to.split("/");
+
+    from = removePrePostFixes(from);
+    to = removePrePostFixes(to);
+
+    for(var i = 0; i < from.length; i++){
+        var subs = splt[i];
+        if(!subs){
+            throw new Error("Can't parse path " + file);
+        }
+
+        if(subs == from[i]){
+            splt[i] = to[i];
+        } else {
+            throw new Error("Can't convert " + path + " with given prefix " + from);
+        }
+    }
+
+    return path.normalize(splt.join("/"));
+}
+
+files.parse = function(fileName){
 
     fs.mkdir("lib", function(err){});
 
     fs.stat(fileName, function(err, file){
         if(file.isFile()){
 
-            parseFile(fileName, function(parsedFile){
+            files._parseFile(fileName, function(parsedFile){
 
-                fileName = changeExtension(fileName);
+                fileName = files._changeExtension(fileName);
 
                 fs.writeFile(fileName, parsedFile, function(err){
                     if(err)
@@ -48,6 +93,4 @@ function parse(fileName){
     });
 }
 
-module.exports = {
-    parse: parse
-}
+module.exports = files;
