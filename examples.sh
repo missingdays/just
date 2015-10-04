d6f3569e1ee8ed76a7758870b2a3abc213a75bf3
missingdays <rebovykin@gmail.com>
2015-10-04 21:42:47 +0300

    Remove old files

--- examples.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-cd examples
-node ../index.js jst js
-cd ..
--- src/main.js
+++
@@ -1,6 +0,0 @@
-var flags = require("flags");
-var files = require("./files/files.js");
-
-var args = process.argv;
-
-files.parse(args[2], args[3]);
